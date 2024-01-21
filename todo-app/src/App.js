f632ea3ebfc0061dbaf245ed317a5b65e01e7c7a
Zhyldyz <jyldyz0610@gmail.com>
2024-01-21 11:28:32 +0100

    Todo React

---
+++ todo-app/src/App.js
@@ -0,0 +1,18 @@
+import TodoList from './TodoList';
+import logo from './logo.svg';
+import './App.css';
+
+
+    function App() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Meine Todo-Liste</h1>
+            <TodoList />
+          </header>
+        </div>
+      );
+    }
+  
+
+export default TodoList;
